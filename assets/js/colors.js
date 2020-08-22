8483979eb4335abbff7882b67770955ce203a294
Oskar <oskarradonkimball@gmail.com>
2020-08-22 01:58:31 -0700

    refactor into modules

---
+++ assets/js/colors.js
@@ -0,0 +1,26 @@
+export let colors = [
+  {
+    "bg": "#dfffe5",
+    "font": "#fe7f78"
+  },
+  {
+    "bg": "#3c6cdf",
+    "font": "#fe7f78"
+  },
+  {
+    "bg": "#ffb3a1",
+    "font": "#000"
+  },
+  {
+    "bg": "#8da49d",
+    "font": "#4663c6"
+  },
+  {
+    "bg": "#828abb",
+    "font": "#5538b4"
+  },
+  {
+    "bg": "#00469d",
+    "font": "#fed767"
+  }
+]
---
+++ assets/js/fonts.js
@@ -0,0 +1,122 @@
+export let fonts = [
+  {
+    "name": "Authentic Sans",
+    "modifier": "60",
+    "designers": "Christina Janus & Desmond Wong",
+    "foundry": "Authentic",
+    "year": "2015",
+    "size": ""
+  },
+  {
+    "name": "Stereo Sans",
+    "modifier": "",
+    "designers": "Joonas Timmi",
+    "foundry": "SUVA Type Foundry",
+    "year": "2017",
+    "size": ""
+  },
+  {
+    "name": "Iosevka",
+    "modifier": "Light Italic",
+    "designers": "Belleve Invis",
+    "foundry": "Typeof.net",
+    "year": "2015",
+    "size": ""
+  },
+  {
+    "name": "Glyph World",
+    "modifier": "Flower",
+    "designers": "Leah Maldonado",
+    "foundry": "glyphworld.online",
+    "year": "2019",
+    "size": ""
+  },
+  {
+    "name": "Wreckage Grotesque",
+    "modifier": "",
+    "designers": "Rory King",
+    "foundry": "rorykingetc.com",
+    "year": "2018",
+    "size": ""
+  },
+  {
+    "name": "Lunchtype 25",
+    "modifier": "",
+    "designers": "Steppot",
+    "foundry": "lunchtype.com",
+    "year": "2016",
+    "size": ""
+  },
+  {
+    "name": "Apfel Grotezk",
+    "modifier": "Brukt",
+    "designers": "Luigi Gorlero",
+    "foundry": "Collletttivo",
+    "year": "2019",
+    "size": ""
+  },
+  {
+    "name": "Kaeru Kaeru",
+    "modifier": "",
+    "designers": "Isabel Motz",
+    "foundry": "Velvetyne",
+    "year": "2019",
+    "size": ""
+  },
+  {
+    "name": "Hershey Noailles",
+    "modifier": "Astrology",
+    "designers": "Allen Vincent Hershey",
+    "foundry": "Luuse",
+    "year": "1967",
+    "size": ""
+  },
+  {
+    "name": "Work Sans",
+    "modifier": "Black",
+    "designers": "Wei Huang",
+    "foundry": "Google Fonts",
+    "year": "2015",
+    "size": ""
+  },
+  {
+    "name": "Quaerite Regnum Dei",
+    "modifier": "",
+    "designers": " 	Fredrick Brennan",
+    "foundry": "",
+    "year": "2019",
+    "size": ""
+  },
+  {
+    "name": "WhoIs Mono",
+    "modifier": "",
+    "designers": "Raphael Bastide",
+    "foundry": "",
+    "year": "2014",
+    "size": ""
+  },
+  {
+    "name": "Infini",
+    "modifier": "Regular",
+    "designers": "Sandrine Nugue",
+    "foundry": "",
+    "year": "2015",
+    "size": ""
+  },
+  {
+    "name": "Passio Graphis",
+    "modifier": "",
+    "designers": "David Rudnick",
+    "foundry": "",
+    "year": "2019",
+    "size": ""
+  },
+  {
+    "name": "Outward",
+    "modifier": "Block",
+    "designers": "Raoul Audouin",
+    "foundry": "Velvetyne",
+    "year": "2019",
+    "size": "big"
+  }
+]
